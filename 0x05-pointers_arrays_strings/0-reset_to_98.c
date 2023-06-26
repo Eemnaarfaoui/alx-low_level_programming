90dfff96f4149eecb369f0ba7ec93bd2bd9fcc1d
Eemnaarfaoui <emnaarfaoui255gmail.com>
2023-06-26 15:40:36 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*reset_to_98 - updates the value pointed to n to 98.
+*
+*@n: Pointer to an integer.
+*
+*no return
+*/
+void reset_to_98(*n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* swap_int - Swaps the values of two integers.
+* @a: Pointer to the first integer.
+* @b: Pointer to the second integer.
+*
+* Description: This function takes two pointers to integers, `a` and `b`,
+*              and swaps the values they point to. The value pointed to by `a`
+*              will be exchanged with the value pointed to by `b`.
+*/
+void swap_int(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+}
